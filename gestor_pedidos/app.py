ea67845ee7c57d10eb2dae220770c88214f9fa60
sebassilv <98719327+Bytestian76@users.noreply.github.com>
2025-11-02 12:48:56 -0500

    Update app.py

--- gestor_pedidos/app.py
+++ gestor_pedidos/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
 import sqlite3
+import os
 
 app = Flask(__name__)
 app.secret_key = 'lunenauditore'
@@ -227,6 +228,8 @@ def eliminar(id):
 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     create_table()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
